06076af7f0eaa21a822263773d94a34428ae4ee4
GerdaBoman <88060352+GerdaBoman@users.noreply.github.com>
2022-01-01 22:48:36 +0100

    Checking if age is entered correctly using while loop

--- SimpleApp-TrainBasic/Program.cs
+++ SimpleApp-TrainBasic/Program.cs
@@ -4,29 +4,44 @@ Console.Write("Enter your first name?: ");
 string? firstName = Console.ReadLine();
 
 Console.Write("Enter your age: ");
-int age = Convert.ToInt32(Console.ReadLine());
+string? ageResponse = Console.ReadLine();
 
+bool ageCheck = int.TryParse(ageResponse,out int age);
+
+
+do
+{
+
+    Console.WriteLine("Invalid age! Try again");
+    Console.Write("Enter your age: ");
+    ageResponse = Console.ReadLine();
+
+    ageCheck = int.TryParse(ageResponse, out age);
+}
+while (ageCheck == false);
 
 
 int yearWait = 20 - age;
-
-
-if(age > 20)
+if (age > 20)
 {
     Console.WriteLine("Welcome");
     if (firstName.StartsWith("g") || firstName.StartsWith("G"))
     {
         Console.WriteLine($"{firstName}, what a nice name you have!");
+
     }
     else
     {
         Console.WriteLine($"Hello {firstName}");
+
     }
 }
-else 
+else
 {
     Console.WriteLine($"{firstName}, come back in {yearWait} years.");
 
+
 }
 
-Console.Read();
+Console.ReadKey();  
+

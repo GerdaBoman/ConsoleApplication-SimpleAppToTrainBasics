3fc97f3b994ca8fcf80cbddbb841ebc9ec7f0856
GerdaBoman <88060352+GerdaBoman@users.noreply.github.com>
2022-01-01 22:10:20 +0100

    Add project files.

---
+++ SimpleApp-TrainBasic/Program.cs
@@ -0,0 +1,32 @@
+﻿DateTime today = DateTime.Now;  
+
+Console.Write("Enter your first name?: ");
+string? firstName = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+
+
+int yearWait = 20 - age;
+
+
+if(age > 20)
+{
+    Console.WriteLine("Welcome");
+    if (firstName.StartsWith("g") || firstName.StartsWith("G"))
+    {
+        Console.WriteLine($"{firstName}, what a nice name you have!");
+    }
+    else
+    {
+        Console.WriteLine($"Hello {firstName}");
+    }
+}
+else 
+{
+    Console.WriteLine($"{firstName}, come back in {yearWait} years.");
+
+}
+
+Console.Read();
